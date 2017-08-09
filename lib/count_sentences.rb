0b7bdf7347d4b85d10d01e6655efca99084ff988
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-08-09 15:46:00 -0400

    done again

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
+
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+def count_sentences
+    split_strings_on_punc = self.split(/[!?.]+/)
+    split_strings_on_punc.length
   end
 end
